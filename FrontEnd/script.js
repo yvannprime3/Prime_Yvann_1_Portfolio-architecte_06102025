21c5df670227a67614240dd8b4f85d1c675c3191
Yvann Prime <yvannprime3@gmail.com>
2025-10-22 13:20:54 +0200

    Boutons filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -8,7 +8,7 @@ fetch("http://localhost:5678/api/works").then(res => {
     console.log(data)
     document.querySelector(".gallery").innerHTML=""
     data.forEach(work => {
-        document.querySelector(".gallery").innerHTML+=`
+        const travaux = document.querySelector(".gallery").innerHTML+=`
         	<figure>
 				<img src="${work.imageUrl}" alt="Abajour Tahina">
 				<figcaption>${work.title}</figcaption>
@@ -16,4 +16,38 @@ fetch("http://localhost:5678/api/works").then(res => {
         `
     });
 })
+})
+
+// Boutons filtres //
+
+const boutonTous = document.querySelector(".btn-tous")
+boutonTous.addEventListener("click", function () {
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.categoryId
+    })
+    console.log(travauxFiltres)
+})
+
+const boutonObjets = document.querySelector(".btn-objets")
+boutonTous.addEventListener("click", function () {
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.categoryId
+    })
+    console.log(travauxFiltres)
+})
+
+const boutonAppartements = document.querySelector(".btn-appartements")
+boutonTous.addEventListener("click", function () {
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.categoryId
+    })
+    console.log(travauxFiltres)
+})
+
+const boutonHotelsRestaurants = document.querySelector(".btn-hotels&restaurants")
+boutonTous.addEventListener("click", function () {
+    const travauxFiltres = travaux.filter(function (travaux) {
+        return travaux.categoryId
+    })
+    console.log(travauxFiltres)
 })
